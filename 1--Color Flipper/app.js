0e9a6b4fd8a52acc492f9622946e354997ae0518
iambariz <61651210+iambariz@users.noreply.github.com>
2020-11-27 13:23:54 +0000

    85% ready
    Just a save, before the last function.

--- 1--Color Flipper/app.js
+++ 1--Color Flipper/app.js
@@ -7,22 +7,63 @@ const hex = [
 
 const colorId = document.getElementById("colorId");
 const mainDiv = document.getElementById("main");
+const hexOption = document.getElementById("Hex");
+const rgbOption = document.getElementById("Rgb");
+const button = document.getElementById("button");
+const rgbRadio = document.getElementById("rgbRadio");
+const hexRadio = document.getElementById("hexRadio");
 
+button.addEventListener('click', makeItColorful);
+hexOption.addEventListener('click', function(rgbOption, hexOption){
+    toggle(rgbOption, hexOption);
+})
 
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-
 function getHex(){
-    var hexValue = "#";
+    let hexValue = "#";
     for(let i=0; i < 6; i++){
         let randomNum = getRandom(0,15);
         hexValue += hex[randomNum];
     }
-    console.log(hexValue);
-    document.body.style.backgroundColor = hexValue;
-    console.log(hexValue);
+    setColor(hexValue);
 }
 
-getHex();
+function getRgb(){
+    let rgbValue = []
+    for(let i=0; i < 3; i++){
+        let generate = getRandom(0,255);
+        rgbValue.push(generate);
+    }
+    function getDone(){
+        rgbValue.splice(0, 0, 'rgb');
+        rgbValue.splice(1, 0, '(');
+        rgbValue.splice(3, 0, ',');
+        rgbValue.splice(5, 0, ',');
+        rgbValue.splice(7, 0, ')');
+    }
+    getDone();
+    rgbValue = rgbValue.join("");
+    setColor(rgbValue);
+}
+
+function setColor(colorCode){
+    document.body.style.backgroundColor = colorCode;
+    colorId.textContent = colorCode;
+}
+
+function makeItColorful(){
+    if(rgbOption.checked === true){
+        getRgb();
+    }
+    if(hexOption.checked === true){
+        getHex();
+    }
+}
+
+function toggle(off, on){
+   off.classList.remove("selected");
+   on.classList.add("selected");
+}
