4bdd1195e51f7d55f5b0761acc67d104db34eff4
iambariz <61651210+iambariz@users.noreply.github.com>
2020-12-01 10:55:14 +0000

    Final Update
    Toggle function added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const buttons = document.querySelectorAll('.fas.fa-arrow-down');
 
-
 buttons.forEach(function(btn){  
     btn.addEventListener('click', function(e){
+        questionBox = document.querySelectorAll('.question-text');
+        questionBox.forEach(function(e){
+            e.classList.remove("open");
+        })
         const container = (e.currentTarget.parentElement.children[3]);
         container.classList.toggle('open');
     });
