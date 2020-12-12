391f39b8c83adc592bfa40ce90af45945b85db05
iambariz <61651210+iambariz@users.noreply.github.com>
2020-12-12 15:53:10 +0000

    JS Update
    Scrolltrack, and some minor HTML & CSS Update

--- 10--Scroll/app.js
+++ 10--Scroll/app.js
@@ -1,7 +1,16 @@
+//Sections
+const home = document.querySelector("#home");
+const about = document.querySelector("#About");
+const services = document.querySelector("#Services");
+const tours = document.querySelector("#Tours");
+
+//Navbar 
+const navContainer = document.querySelector(".navbar")
 const closeBtn = document.querySelector(".fas.fa-times");
 const openBtn = document.querySelector(".fas.fa-bars");
 const navbar = document.querySelector(".nav-ul");
 
+//Navbar toggle
 toggleOpen = () => {
     console.log("asd")
     navbar.classList.toggle("open");
@@ -9,8 +18,27 @@ toggleOpen = () => {
     console.log(closeBtn.classList);
 }
 
+
+
+//Even listeners
+
 openBtn.addEventListener('click', toggleOpen);
 openBtn.addEventListener('click', toggleOpen);
+
+
+window.addEventListener("scroll", ()=>{
+    let windowVar = window.pageYOffset;
+        console.log("works");
+    if(home.offsetTop + 100 <= windowVar){
+        navContainer.classList.add("scrolled");
+    }
+    else{
+        navContainer.classList.remove("scrolled");
+    }
+});
+
+    console.log(tours);
+
 
 
 
