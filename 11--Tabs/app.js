46090c57a385009529b3a06a156215e10ed4d7f2
iambariz <61651210+iambariz@users.noreply.github.com>
2020-12-15 12:11:26 +0000

    Final update
    JS Working

--- 11--Tabs/app.js
+++ 11--Tabs/app.js
@@ -1,19 +1,30 @@
 const buttons = document.querySelectorAll(".tab-btn");
-const shit = document.querySelector("#shit");
+const displayItems = document.querySelectorAll(".tab-item");
 
-shit.addEventListener("click", function(){
-    console.log("asd");
-  }); 
-
-console.log(shit);
-console.log(buttons);
+console.log(displayItems);
 
 buttons.forEach(item => {
     item.addEventListener('click', e => {
-        console.log("asd");
         if(e.currentTarget.dataset.id == "one"){
-            console.log("works");
+            //console.log(`${e.currentTarget.dataset.id}`);
+            toDisplay(displayItems[0], displayItems[1], displayItems[2])
+        }
+        if(e.currentTarget.dataset.id == "two"){
+            //console.log(`${e.currentTarget.dataset.id}`);
+            toDisplay(displayItems[1], displayItems[2], displayItems[0])
+        }
+        if(e.currentTarget.dataset.id == "three"){
+            //console.log(`${e.currentTarget.dataset.id}`);
+            toDisplay(displayItems[2], displayItems[1], displayItems[0])
         }
     })
 })
 
+
+toDisplay = (add, ...args) => {
+    add.classList.add("open");
+    args.forEach(element => {
+        element.classList.remove("open");
+        //console.log(element);
+    });
+}
