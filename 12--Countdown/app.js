a3d4905ee8a6b57b93b360772502afd3d31baba2
iambariz <61651210+iambariz@users.noreply.github.com>
2021-01-25 13:19:22 +0000

    Update JS
    Starting items added

---
+++ 12--Countdown/app.js
@@ -0,0 +1,26 @@
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+const weekdays = [
+    "Monday",
+    "Tuesday",
+
+]
+
+const deadline = document.querySelector(".end-container");
+const giveaway = document.querySelector(".end-text");
+const items = document.querySelectorAll(".deadline-format h4");
+
+console.log(items);
