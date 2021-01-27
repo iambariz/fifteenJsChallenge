8b56129521ee596f708a3b8b42f17bab5c24a542
iambariz <61651210+iambariz@users.noreply.github.com>
2021-01-27 22:44:25 +0000

    Update JS
    If statement added, alert MSG developing in progress

--- 14--Grocery-Bud/app.js
+++ 14--Grocery-Bud/app.js
@@ -8,6 +8,11 @@ const clearBtn = document.querySelector('.clear-btn');
 
 let edit = false;
 let items = [];
+let messages = [
+    "Item added",
+    "Item removed",
+    "Please enter a name"
+]
 
 //Event listeners
 button.addEventListener('click', function (e) {
@@ -18,24 +23,33 @@ button.addEventListener('click', makeItem);
 //Functions
 
 function makeItem() {
-    //Create & add classes
-    let item = document.createElement("div");
-    item.classList.add('item');
-    let paragraph = document.createElement("p");
-    paragraph.textContent = textField.value;
-    let buttonDiv = document.createElement("div");
-    buttonDiv.classList.add('buttons');
-    let editBtn = document.createElement("i");
-    editBtn.classList.add('fas', 'fa-user-edit');
-    let delBtn = document.createElement("i");
-    delBtn.classList.add('fas', 'fa-trash-alt');
-    //Append
-    buttonDiv.appendChild(editBtn);
-    buttonDiv.appendChild(delBtn);
-    item.appendChild(paragraph);
-    item.appendChild(buttonDiv);
-    itemsContainer.appendChild(item);
-    //Push into an array
-    items.push(item);
-    console.log(items);
+    if (textField.value.length > 0) {
+        //Create & add classes
+        let item = document.createElement("div");
+        item.classList.add('item');
+        let paragraph = document.createElement("p");
+        paragraph.textContent = textField.value;
+        let buttonDiv = document.createElement("div");
+        buttonDiv.classList.add('buttons');
+        let editBtn = document.createElement("i");
+        editBtn.classList.add('fas', 'fa-user-edit');
+        let delBtn = document.createElement("i");
+        delBtn.classList.add('fas', 'fa-trash-alt');
+        //Append
+        buttonDiv.appendChild(editBtn);
+        buttonDiv.appendChild(delBtn);
+        item.appendChild(paragraph);
+        item.appendChild(buttonDiv);
+        itemsContainer.appendChild(item);
+        //Push into an array
+        items.push(item);
+        console.log(items);
+    } else {
+        alert("Works");
+    }
+}
+
+function displayAlert(msg) {
+    notiBar.textContent = msg;
+
 }
