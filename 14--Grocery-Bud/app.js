0a47c2d4e96a7cb279505b1e0caa1a8ca83c1d0d
iambariz <61651210+iambariz@users.noreply.github.com>
2021-01-27 20:17:10 +0000

    JS Update
    variables done

---
+++ 14--Grocery-Bud/app.js
@@ -0,0 +1,10 @@
+//Selectors
+
+const notiBar = document.querySelector('.notification-msg');
+const textField = document.querySelector('#grocery');
+const button = document.querySelector('.btn-submit');
+const itemsContainer = document.querySelector('.items');
+const clearBtn = document.querySelector('.clear-btn');
+
+let edit = false;
+let items = [];
