5f302330764ac59f4df2e1d1d33b3ec37465e1f7
iambariz <61651210+iambariz@users.noreply.github.com>
2021-01-30 20:00:01 +0000

    Update JS
    Algorithm works

--- 15--Slider/app.js
+++ 15--Slider/app.js
@@ -2,14 +2,44 @@
 
 const items = document.querySelectorAll('.slide-item');
 const nextButton = document.querySelector('.next');
-const previousButton = document.querySelector('.previous');
+const prevButton = document.querySelector('.previous');
 //console.log(items);
 
-nextButton.addEventListener('click', slideIt);
+nextButton.addEventListener('click', slideUp);
+prevButton.addEventListener('click', slideDown);
 
 //Additional variables
 let i = 0;
 
-function slideIt() {
+function slideUp() {
+    i++;
+    items.forEach(function (item) {
+        item.style.transform = "translateX(" + i + "00%)";
+    });
+    buttonDisplay();
+}
 
+function slideDown() {
+    i--;
+    items.forEach(function (item) {
+        item.style.transform = "translateX(" + i + "00%)";
+    });
+    buttonDisplay();
+}
+
+function buttonDisplay() {
+    switch (i) {
+        case 0:
+            prevButton.style.display = "none";
+            break;
+        case 1:
+            prevButton.style.display = "inline";
+            break;
+        case 2:
+            nextButton.style.display = "inline";
+            break;
+        case 3:
+            nextButton.style.display = "none";
+            break;
+    }
 }
