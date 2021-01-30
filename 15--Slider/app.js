c04e0e4403e94192519292745275decbf4aff590
iambariz <61651210+iambariz@users.noreply.github.com>
2021-01-30 19:02:10 +0000

    HTML & CSS Update
    Little fix

---
+++ 15--Slider/app.js
@@ -0,0 +1,15 @@
+//Selectors
+
+const items = document.querySelectorAll('.slide-item');
+const nextButton = document.querySelector('.next');
+const previousButton = document.querySelector('.previous');
+//console.log(items);
+
+nextButton.addEventListener('click', slideIt);
+
+//Additional variables
+let i = 0;
+
+function slideIt() {
+
+}
